d48a672c1e8b781e0e305b5a582b51abdb163064
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-18 12:13:47 +0100

    Fix DetailTravel

--- src/pages/DetailTravel.jsx
+++ src/pages/DetailTravel.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
 import travels from "../data/travels";
@@ -12,46 +12,44 @@ export default function DetailTravel() {
 	const travelUsers = users.filter((user) => user.travel_id === travelId);
 
 	const { list } = useTravels();
-	useEffect();
 	const travelName = list.find((current) => current.id === travelId);
-	console.log(travelName);
 
 	// stato iniziale: mostro tutti gli utenti già filtrati per il singolo viaggio
 	const [displayedUsers, setDisplayedUsers] = useState(travelUsers);
 
 	return (
-		<>
-			<div className="container mt-3">
-				<SearchBar users={displayedUsers} onSearchResults={setDisplayedUsers} />
-				<h1 className="d-flex justify-content-center">{travelName} trip</h1>
-				{/* table */}
-				<table className="table border-dark">
-					{/* table head */}
-					<thead>
-						<tr>
-							<th scope="col">#</th>
-							<th scope="col">Firstname</th>
-							<th scope="col">Lastname</th>
-							<th scope="col">E-mail</th>
-							<th scope="col">Phone number</th>
-							<th scope="col">id_code</th>
+		<div className="container mt-3">
+			<SearchBar users={displayedUsers} onSearchResults={setDisplayedUsers} />
+			<h1 className="d-flex justify-content-center">
+				{travelName.destination} trip
+			</h1>
+			{/* table */}
+			<table className="table border-dark">
+				{/* table head */}
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Firstname</th>
+						<th scope="col">Lastname</th>
+						<th scope="col">E-mail</th>
+						<th scope="col">Phone number</th>
+						<th scope="col">id_code</th>
+					</tr>
+				</thead>
+				{/* table body */}
+				<tbody>
+					{displayedUsers.map((user) => (
+						<tr key={user.id}>
+							<th>{user.id}</th>
+							<td>{user.first_name}</td>
+							<td>{user.last_name}</td>
+							<td>{user.email}</td>
+							<td>{user.phone}</td>
+							<td>{user.id_code}</td>
 						</tr>
-					</thead>
-					{/* table body */}
-					<tbody>
-						{displayedUsers.map((user) => (
-							<tr key={user.id}>
-								<th>{user.id}</th>
-								<td>{user.first_name}</td>
-								<td>{user.last_name}</td>
-								<td>{user.email}</td>
-								<td>{user.phone}</td>
-								<td>{user.id_code}</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</>
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 }
